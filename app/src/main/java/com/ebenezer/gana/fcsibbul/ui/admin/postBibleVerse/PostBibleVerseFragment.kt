8cba76d4c1bb2c9075558c705e484f7eec333fe9
Gana Ebenezer <ebenezergana@gmail.com>
2022-08-16 21:03:10 +0100

    renamed BibleVerseFragment to PostBibleVerseFragment

--- app/src/main/java/com/ebenezer/gana/fcsibbul/ui/admin/postBibleVerse/PostBibleVerseFragment.kt
+++ app/src/main/java/com/ebenezer/gana/fcsibbul/ui/admin/postBibleVerse/PostBibleVerseFragment.kt
@@ -1,22 +1,21 @@
 package com.ebenezer.gana.fcsibbul.ui.admin.postBibleVerse
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.ebenezer.gana.fcsibbul.R
 
-class BibleVerseFragment : Fragment() {
+class PostBibleVerseFragment : Fragment() {
 
-    private lateinit var viewModel: BibleVerseViewModel
+    private lateinit var viewModel: PostBibleVerseViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_bible_verse, container, false)
+        return inflater.inflate(R.layout.fragment_post_bible_verse, container, false)
     }
 
 
--- app/src/main/java/com/ebenezer/gana/fcsibbul/ui/admin/postBibleVerse/PostBibleVerseViewModel.kt
+++ app/src/main/java/com/ebenezer/gana/fcsibbul/ui/admin/postBibleVerse/PostBibleVerseViewModel.kt
@@ -2,6 +2,6 @@ package com.ebenezer.gana.fcsibbul.ui.admin.postBibleVerse
 
 import androidx.lifecycle.ViewModel
 
-class BibleVerseViewModel : ViewModel() {
+class PostBibleVerseViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
