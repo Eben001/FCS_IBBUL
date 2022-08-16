99ab4e3d72cd03c8758f6fc38ecbcb8fa3f6e6cc
Gana Ebenezer <ebenezergana@gmail.com>
2022-08-16 21:14:17 +0100

    navigate to DailyBibleVerse fragment when it is selected from the navigation menu

--- app/src/main/java/com/ebenezer/gana/fcsibbul/ui/host/HostActivityLoggedIn.kt
+++ app/src/main/java/com/ebenezer/gana/fcsibbul/ui/host/HostActivityLoggedIn.kt
@@ -75,7 +75,7 @@ class HostActivityLoggedIn : AppCompatActivity(), NavigationView.OnNavigationIte
             }
             R.id.daily_guide -> navController.navigate(R.id.navigation_daily_guide)
 
-            R.id.bibleVerseFragment -> Toast.makeText(this, "Bible Quotes", Toast.LENGTH_SHORT).show()
+            R.id.bibleVerseFragment -> navController.navigate(R.id.navigation_daily_bible_verse)
             R.id.fcs_song -> navController.navigate(R.id.navigation_fcs_song)
             R.id.admin_login -> navController.navigate(R.id.nav_graph_admin)
             R.id.contact_us -> dialogsNavigator.showContactusDialog()
